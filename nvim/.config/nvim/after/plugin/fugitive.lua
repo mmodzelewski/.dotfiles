e6423ab8340a57be0f199f1096807426fb0c6e02
Maciej Modzelewski <maciej@modzelewski.dev>
2023-04-19 21:46:38 +0200

    Update nvim remaps

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "gs", vim.cmd.Git);
+vim.keymap.set("n", "gnb", function ()
+    vim.cmd.Git("checkout -b " .. vim.fn.input("New branch name: "));
+end);
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -6,18 +6,24 @@ lsp.preset('recommended')
 local cmp = require('cmp')
 
 cmp.setup({
-  mapping = {
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }
+    mapping = {
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    }
 })
 
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("i", "<C-q>", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "ge", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "<leader>tn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>tf", function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
 end)
 
 lsp.setup()
@@ -25,4 +31,3 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true,
 })
-
